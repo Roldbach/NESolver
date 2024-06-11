295c2ac8878f09681c963c1b7ca656624b36fe28
Roldbach <roldbach@yahoo.com>
2024-06-11 15:11:13 +0100

    Clean unnecessary files

--- solver_pipeline_temp.py
+++ solver_pipeline_temp.py
@@ -24,19 +24,17 @@ from utils import matrix_utils
 from utils import optimisation_utils
 from utils import pipeline_utils
 
-np.random.seed(0)
-torch.manual_seed(0)
 
 data_pack = io_utils.load_array_dictionary('./data/Na-K-Cl_simulated_clean.npz')
-pipeline = pipeline_utils.NovelNumericalAgentTrainingPipeline(
+pipeline = pipeline_utils.NeuralNetworkAgentTrainingPipeline(
     data_pack['charge'],
     data_pack['ion_size'],
     data_pack['concentration'],
     data_pack['potential'],
     slice(10),
     slice(1000,1100),
-    1e-3,
-    optim.Adam,
+    1e-4,
+    optim.AdamW,
     nn.MSELoss,
 )
 network, training_loss, validation_loss = pipeline.train()
@@ -44,13 +42,13 @@ plt.plot([i for i in range(len(training_loss))], np.log10(training_loss))
 plt.plot([i for i in range(len(validation_loss))], np.log10(validation_loss))
 plt.show()
 
-pipeline = pipeline_utils.NumericalAgentEvaluationPipeline(
+pipeline = pipeline_utils.NeuralNetworkAgentEvaluationPipeline(
     network,
     data_pack['selectivity_coefficient'],
     data_pack['slope'],
     data_pack['drift'],
-    data_pack['concentration'][1100:1200,:],
-    data_pack['activity'][1100:1200,:],
-    data_pack['potential'][1100:1200,:],
+    data_pack['concentration'][1000:1100,:],
+    data_pack['activity'][1000:1100,:],
+    data_pack['potential'][1000:1100,:],
 )
 pipeline.evaluate()
--- test4.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-from utils import io_utils
-
-
-data_pack = io_utils.load_array_dictionary('./data/Na-K-Mg-Ca-Cl_simulated_noisy_high.npz')
-print(data_pack['drift'])
-print(data_pack['slope'])
