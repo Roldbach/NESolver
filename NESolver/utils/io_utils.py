f03591d07d3f86bfd53385e77b32530f25eff39c
Roldbach <roldbach@yahoo.com>
2024-06-19 20:58:54 +0100

    Remove unused code and refactor for simplicity

--- NESolver/utils/io_utils.py
+++ NESolver/utils/io_utils.py
@@ -1,4 +1,4 @@
-"""A module that handles file loading and saving in the project.
+"""An utility module that handles file loading and saving in the project.
 
 Author: Weixun Luo
 Date: 01/04/2023
@@ -8,16 +8,11 @@ import scipy
 import torch
 from torch import nn
 
-from utils import matrix_utils
-
 
 """ ----- File Loading -----"""
 # {{{ load_array_dictionary
 def load_array_dictionary(file_path: str) -> dict:
-    """Load multiple arrays from the file."""
-    file_content = np.load(file_path)
-    file_content = dict(file_content)
-    return file_content
+    return dict(np.load(file_path))
 # }}}
 
 # {{{ load_state_dictionary
@@ -30,13 +25,7 @@ def load_state_dictionary(file_path: str, module: nn.Module) -> nn.Module:
 """ ----- File Saving -----"""
 # {{{ save_array_dictionary
 def save_arrray_dictionary(file_content: dict, file_path: str) -> None:
-    """Save multiple arrays into the same .npz file."""
     np.savez(file_path, **file_content)
-# }}}
-
-# {{{ save_array_dictionary_matlab
-def save_array_dictionary_matlab(file_content: dict, file_path: str) -> None:
-    scipy.io.savemat(file_path, file_content)
 # }}}
 
 # {{{ save_state_dictionary
